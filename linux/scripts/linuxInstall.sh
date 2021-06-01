348260d327968e88e909e9016ca2c646c7d04af2
bhavik <bpatel5493@gmail.com>
2021-06-01 12:37:07 -0500

    update

--- linux/scripts/linuxInstall.sh
+++ linux/scripts/linuxInstall.sh
@@ -16,49 +16,22 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Install 
-echo "
-██╗███╗░░██╗░██████╗████████╗░█████╗░██╗░░░░░██╗░░░░░██╗███╗░░██╗░██████╗░
-██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░░░██║░░░░░██║████╗░██║██╔════╝░
-██║██╔██╗██║╚█████╗░░░░██║░░░███████║██║░░░░░██║░░░░░██║██╔██╗██║██║░░██╗░
-██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║██║░░░░░██║░░░░░██║██║╚████║██║░░╚██╗
-██║██║░╚███║██████╔╝░░░██║░░░██║░░██║███████╗███████╗██║██║░╚███║╚██████╔╝
-╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░"
+echo "INSTALLING"
 sudo apt-get install -y unzip htop pdftk curl papirus-icon-theme xfce4-terminal wget gpick fonts-powerline build-essential git gimp zsh flameshot ulauncher redis-tools calibre tree qemu-kvm timeshift gnome-shell-extensions gnome-tweaks code signal-desktop dconf-editor
 
 # AWS
-echo "
-░█████╗░░██╗░░░░░░░██╗░██████╗
-██╔══██╗░██║░░██╗░░██║██╔════╝
-███████║░╚██╗████╗██╔╝╚█████╗░
-██╔══██║░░████╔═████║░░╚═══██╗
-██║░░██║░░╚██╔╝░╚██╔╝░██████╔╝
-╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═════╝░
-"
+echo "AWS"
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 # Node JS
-echo "
-███╗░░██╗░█████╗░██████╗░███████╗
-████╗░██║██╔══██╗██╔══██╗██╔════╝
-██╔██╗██║██║░░██║██║░░██║█████╗░░
-██║╚████║██║░░██║██║░░██║██╔══╝░░
-██║░╚███║╚█████╔╝██████╔╝███████╗
-╚═╝░░╚══╝░╚════╝░╚═════╝░╚══════╝
-"
+echo "NODE JS"
 mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 # Docker
-echo "
-██████╗░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░
-██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗
-██║░░██║██║░░██║██║░░╚═╝█████═╝░█████╗░░██████╔╝
-██║░░██║██║░░██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
-██████╔╝╚█████╔╝╚█████╔╝██║░╚██╗███████╗██║░░██║
-╚═════╝░░╚════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
-"
+echo "DOCKER"
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
@@ -66,64 +39,34 @@ sudo apt update
 sudo apt install docker-ce
 
 # Typora
-echo "
-████████╗██╗░░░██╗██████╗░░█████╗░██████╗░░█████╗░
-╚══██╔══╝╚██╗░██╔╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
-░░░██║░░░░╚████╔╝░██████╔╝██║░░██║██████╔╝███████║
-░░░██║░░░░░╚██╔╝░░██╔═══╝░██║░░██║██╔══██╗██╔══██║
-░░░██║░░░░░░██║░░░██║░░░░░╚█████╔╝██║░░██║██║░░██║
-░░░╚═╝░░░░░░╚═╝░░░╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
-"
+echo "TYPORA"
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt-get update
 sudo apt-get install typora
 
 #NVM
-echo "
-███╗░░██╗██╗░░░██╗███╗░░░███╗
-████╗░██║██║░░░██║████╗░████║
-██╔██╗██║╚██╗░██╔╝██╔████╔██║
-██║╚████║░╚████╔╝░██║╚██╔╝██║
-██║░╚███║░░╚██╔╝░░██║░╚═╝░██║
-╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░░░░╚═╝
-"
+echo "NVM"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+
 nvm install v14.15.3
 
 # oh-my-zsh 
-echo "
-░█████╗░██╗░░██╗░░░░░░███╗░░░███╗██╗░░░██╗░░░░░░███████╗░██████╗██╗░░██╗
-██╔══██╗██║░░██║░░░░░░████╗░████║╚██╗░██╔╝░░░░░░╚════██║██╔════╝██║░░██║
-██║░░██║███████║█████╗██╔████╔██║░╚████╔╝░█████╗░░███╔═╝╚█████╗░███████║
-██║░░██║██╔══██║╚════╝██║╚██╔╝██║░░╚██╔╝░░╚════╝██╔══╝░░░╚═══██╗██╔══██║
-╚█████╔╝██║░░██║░░░░░░██║░╚═╝░██║░░░██║░░░░░░░░░███████╗██████╔╝██║░░██║
-░╚════╝░╚═╝░░╚═╝░░░░░░╚═╝░░░░░╚═╝░░░╚═╝░░░░░░░░░╚══════╝╚═════╝░╚═╝░░╚═╝
-"
+echo "OH-MY-ZSH"
 sudo adduser bhavik kvm
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s $(which zsh)
 
 # Clean Up
-echo "
-░█████╗░██╗░░░░░███████╗░█████╗░███╗░░██╗██╗░░░██╗██████╗░
-██╔══██╗██║░░░░░██╔════╝██╔══██╗████╗░██║██║░░░██║██╔══██╗
-██║░░╚═╝██║░░░░░█████╗░░███████║██╔██╗██║██║░░░██║██████╔╝
-██║░░██╗██║░░░░░██╔══╝░░██╔══██║██║╚████║██║░░░██║██╔═══╝░
-╚█████╔╝███████╗███████╗██║░░██║██║░╚███║╚██████╔╝██║░░░░░
-░╚════╝░╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░
-"
+echo "CLEAN UP"
 rm awscli-bundle.zip
 rm -rf awscli-bundle
 
-echo "
-██████╗░░█████╗░███╗░░██╗███████╗
-██╔══██╗██╔══██╗████╗░██║██╔════╝
-██║░░██║██║░░██║██╔██╗██║█████╗░░
-██║░░██║██║░░██║██║╚████║██╔══╝░░
-██████╔╝╚█████╔╝██║░╚███║███████╗
-╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
-"
+# Add global git configurations here
+echo "GIT CONFIG"
+# TO DO
+
+echo "DONE"
